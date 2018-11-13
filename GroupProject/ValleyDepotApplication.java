c597fa50f3215549edd8daee9beb844453b36ce4
coffmant23 <44989339+coffmant23@users.noreply.github.com>
2018-11-13 16:06:43 -0500

    Update ValleyDepotApplication.java

--- GroupProject/ValleyDepotApplication.java
+++ GroupProject/ValleyDepotApplication.java
@@ -18,9 +18,9 @@ public class ValleyDepotApplication {
         int choiceSelector = 0;
         
         customerList[0]= new Customer("Nick","Coffman","12 apple wood","555-555-5555","myemail@dukes.com",0);
-        customerList[1]= new Customer("John","Good","12 apple wood","555-555-5555","myemail@dukes.com",0);
-        customerList[2]= new Customer("Adam","Zing","12 apple wood","555-555-5555","myemail@dukes.com",0);
-        customerList[3]= new Customer("Chance","ay","12 apple wood","555-555-5555","myemail@dukes.com",0);
+        customerList[1]= new Customer("John","Good","12 apple wood","555-555-5555","myemail@dukes.com",1);
+        customerList[2]= new Customer("Adam","Zing","12 apple wood","555-555-5555","myemail@dukes.com",2);
+        customerList[3]= new Customer("Chance","ay","12 apple wood","555-555-5555","myemail@dukes.com",3);
         
         do
         {
