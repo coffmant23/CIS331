06919be6ea89ae058accc5d0d1b3c6fd3840044d
coffmant23 <44989339+coffmant23@users.noreply.github.com>
2018-11-13 23:07:27 -0500

    Update Vendor.java

--- GroupProject/Vendor.java
+++ GroupProject/Vendor.java
@@ -7,15 +7,34 @@ public class Vendor {
     public String name;
     private String address;
     private String phoneNumber;
+    public String vendorId; 
+    public static int counter;
     
-    public Vendor(Item itemSold, String name, String address, String number)
+    public Vendor(String name, String address, String phoneNumber)
     {
-        this.name = name;
-        this.addresss = address;
-        this.number = number;
+            this.name =name;
+            this.address = address;
+            this.phoneNumber = phoneNumber;
+            this.vendorId= "v"+counter;
+            counter++;
     }
     
+     public void setName(String name )
+    {
+        this.name = name;
+    }
     
+     public void setAddress(String address )
+    {
+        this.address = address;
+    }
+     
+     public void setPhoneNumber(String phoneNumber )
+    {
+        this.address = phoneNumber;
+    }
+
     
     
 }
+
