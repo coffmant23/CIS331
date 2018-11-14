ee2f12ae1deb848b6614ea8a86886d908704ffd5
shuuv <45016543+shuuv@users.noreply.github.com>
2018-11-14 04:15:51 -0500

    Update Sale.java

--- GroupProject/Sale.java
+++ GroupProject/Sale.java
@@ -2,16 +2,29 @@
 package GroupProject;
 
 public class Sale {
-    
-    public Item item;
+    public Item itemSold;
+    public String saleID;
     public int quantity;
     public String date;
-    public Customer customer;
+    public String custID;
+    public int transaction;
+
     
 
+    public Sale(Item itemSold, int quantity, String date, String custID, int transaction,int saleTracker){
+        this.quantity = quantity;
+        this.itemSold = itemSold;
+        this.date = date;
+        this.custID = custID;
+        this.transaction = transaction;
+        this.saleID = "s" + saleTracker;
+    }
 
-
-
+    public void printSale(){//might need to fix the formatting here
+        double quantity = itemSold.quantity;
+        double total = quantity* itemSold.salePrice;
+        System.out.printf("%-11s\t%-5.0f\t$%-10.2f\t-8.2f\t%-8s",itemSold.itemName,quantity,itemSold.salePrice,total,date);
+    }
 
     
 }
