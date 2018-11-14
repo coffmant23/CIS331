7679f5c49e38828759485c5ecdf1158f37271d81
coffmant23 <44989339+coffmant23@users.noreply.github.com>
2018-11-13 22:08:58 -0500

    Update Item.java

--- GroupProject/Item.java
+++ GroupProject/Item.java
@@ -1,4 +1,5 @@
 
+
 package GroupProject;
 
 
@@ -10,7 +11,8 @@ public class Item  {
    public String description;
    private double pricePaid;
    private double salePrice;
-   
+   public String itemId;
+   public static int counter;
    
    
    public Item(String itemName, double weight, String description,
@@ -21,13 +23,36 @@ public class Item  {
    this.description=description;
    this.pricePaid=pricePaid;
    this.salePrice=salePrice;
+   this.itemId = "i"+counter; 
+   counter++;
    }
  
-  
+   
+   public void setItemName(String itemName )
+    {
+        this.itemName = itemName;
+    }
+   
+     public void setWeight(double weight )
+    {
+        this.weight = weight;
+    }
+     public void setDescription(String description)
+    {
+        this.description = description;
+    }
 
-     
-     
+         public void setPricePaid(double pricePaid)
+    {
+        this.pricePaid = pricePaid;
+    }
+         public void setSalePrice(double salePrice)
+    {
+        this.salePrice = salePrice;
+    }
   
 } 
    
+
+
 
